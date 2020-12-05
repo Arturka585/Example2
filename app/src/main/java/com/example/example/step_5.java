5a90e948d95954d29f75b245bd6eb94784908250
user13 <dsfghvj>
2020-12-05 12:05:53 +0500

    что то изменино

--- app/src/main/java/com/example/example/step_5.java
+++ app/src/main/java/com/example/example/step_5.java
@@ -1,20 +1,54 @@
 package com.example.example;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class step_5 extends AppCompatActivity {
+
+    SharedPreferences sPrefs;
+
+    int a = 0;
+    String text;
+
+    FileOutputStream fos = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_5);
 
+        Button next = (Button) findViewById(R.id.next_button);
+        EditText height = (EditText) findViewById(R.id.height);
+        EditText weight = (EditText) findViewById(R.id.weight);
 
-        Window w=getWindow();
-        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = weight.getText().toString();
+                try {
+                    FileOutputStream fileOutput = openFileOutput("weigh_height.txt", MODE_PRIVATE);
+                    fileOutput.write(text.getBytes());
+                    fileOutput.close();
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
